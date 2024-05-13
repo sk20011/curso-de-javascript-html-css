ef1b76c84254a4ccbca0cbf35501bd44685528cc
Solomon <solomonkasparov2001@gmail.com>
2024-05-13 12:52:14 +0200

    Guardado v3.0

---
+++ primer-ejercicio-javaScript/index.js
@@ -0,0 +1,5 @@
+//Creamos un a fucion llamada saludar y le ponemos un  alaerta  que saldra cuando hemos pulsado el boton de saludar y saldra erl texto de Hola Mundo!
+function saludar(){
+    alert( "Hola Mundo!"); 
+
+}
